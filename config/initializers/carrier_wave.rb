57e10254a54f6bdcaa2da376881ce22a6a87691f
xojan0120 <xojan0120@gmail.com>
2018-11-04 20:41:07 +0900

    s3 test

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :path_style            => true
+      :path_style            => false
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
