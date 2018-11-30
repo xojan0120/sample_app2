d73421b0424a9c01114a283da5f0b856f381711e
xojan0120 <xojan0120@gmail.com>
2018-11-30 21:33:57 +0900

    carrier_wave.rb更新3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :path_style            => true               
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      # :path_style            => true               
       # 最初↑がないとheroku環境で画像が表示されなかったが、なぜか一度このオプションを
       # つけて、表示されるようなってから、また、これをはずしても表示されるようになった。謎。
     }
