0514199c46aa7a0eb270f63565b047252cad781c
xojan0120 <xojan0120@gmail.com>
2019-01-12 21:45:09 +0900

    [Add] Capybara+Javascript動作確認完了。wait_for_ajaxスニペット追加。

--- spec/features/multiple_posts_spec.rb
+++ spec/features/multiple_posts_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "MultiplePosts", type: :feature do
-  scenario "てすと", js: true do
+  scenario "Capybara+Javascript設定動作確認", js: true do
     user1 = FactoryBot.create(:user)
     user2 = FactoryBot.create(:user)
+    expect(user1.following?(user2)).to be_falsey
 
     visit root_path
+
     click_link "Log in"
     fill_in "Email", with: "user1@gmail.com"
     fill_in "Password", with: "foobar"
@@ -13,10 +15,15 @@ RSpec.feature "MultiplePosts", type: :feature do
 
     click_link "Users"
     click_link user2.name
-    click_button "Follow"
-    debugger
-    click_button "Unfollow"
-    debugger
+ 
+    expect {
+      click_button "Follow"
+      wait_for_ajax
+    }.to change(user1.following, :count).by(1)
 
+    expect {
+      click_button "Unfollow"
+      wait_for_ajax
+    }.to change(user1.following, :count).by(-1)
   end
 end
