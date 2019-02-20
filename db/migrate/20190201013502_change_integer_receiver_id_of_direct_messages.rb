d3e5e73b82c48cb6d15c3f152e0148ce7575d1fd
xojan0120 <xojan0120@gmail.com>
2019-02-20 16:50:11 +0900

    [Fix] migration file修正

--- db/migrate/20190201013502_change_integer_receiver_id_of_direct_messages.rb
+++ db/migrate/20190201013502_change_integer_receiver_id_of_direct_messages.rb
@@ -1,8 +1,44 @@
-class ChangeIntegerReceiverIdOfDirectMessages < ActiveRecord::Migration[5.1]
-  def up
-    change_column :direct_messages, :receiver_id, :integer
+# used to alter columns in postgresql
+module AlterColumn
+  def alter_column(table_name, column_name, new_type, mapping, default = nil)
+    drop_default = %Q{ALTER TABLE #{table_name} ALTER COLUMN #{column_name} DROP DEFAULT;}
+    execute(drop_default)
+    # puts drop_default
+
+    base = %Q{ALTER TABLE #{table_name} ALTER COLUMN #{column_name} TYPE #{new_type} }
+    if mapping.kind_of?(Hash)
+      contains_else = mapping.has_key?("else")
+      else_mapping = mapping.delete("else")
+      when_mapping = mapping.map { |k, v| "when '#{k}' then #{v}" }.join("\n")
+      
+      base += %Q{ USING CASE #{column_name} #{when_mapping} } unless when_mapping.blank?
+      base += %Q{ ELSE #{else_mapping} } unless contains_else.blank?
+      base += %Q{ END } if !when_mapping.blank? or !contains_else.blank?
+    elsif mapping.kind_of?(String)
+      base += mapping
+    end
+    base += ";"
+    
+    execute(base);
+    # puts base
+    
+    unless default.blank?
+      set_default = %Q{ALTER TABLE #{table_name} ALTER COLUMN #{column_name} SET DEFAULT #{default};}
+      execute(set_default)
+      # puts set_default
+    end
   end
-  def down
-    change_column :direct_messages, :receiver_id, :string
+  module_function :alter_column
+end
+
+class ChangeIntegerReceiverIdOfDirectMessages < ActiveRecord::Migration[5.1]
+  class << self
+    include AlterColumn
+  end
+  def self.up
+    alter_column :direct_messages, :receiver_id, :integer, "USING CAST(receiver_id AS integer)", 1
+  end
+  def self.down
+    raise ActiveRecord::IrreversibleMigration.new
   end
 end
