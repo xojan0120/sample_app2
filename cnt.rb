74e06f78cb531cc54a8c786be8275cc48786acbb
xojan0120 <xojan0120@gmail.com>
2019-02-24 07:14:31 +0900

    [Add] フォロワー通知機能システムテストファイル追加

--- cnt.rb
+++
@@ -1,7 +0,0 @@
-def cnt(model)
-  puts "#{model.to_s} => #{model.count}"
-end
-
-[User,Relationship,Micropost,DirectMessage,DirectMessageStat,UserRoom].each do |model|
-  cnt(model)
-end
---
+++ spec/system/follower_notification_test.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.feature "FollowerNotificationTest", type: :system do
+
+
+end
